8265c8c75be88bb77c20901a8954321f208d68da
saadberrada <saadberrada@hotmail.com>
2017-01-30 11:43:02 +0100

    commit

--- assets/ex1.js
+++ assets/ex1.js
@@ -1,11 +1,21 @@
+var app = angular.module('myApp', []);
+app.controller('monControl', ['$scope', function($scope) {
 
-angular.module('myApp', []).controller('monControl', function($scope) {
-   
 //show a list
-$scope.maVariable = 'TESTA' ;
+$scope.maVariable = 'TEST ANGULARJS' ;
 var mesFruitsTab=["banane","pomme", "ananas","pêche","fraise"] ;
 //$scope.mesFruits=mesFruitsTab;
 
+
+//test check input
+$scope.check =function() {
+	if ($scope.checked){
+		console.log("checked");
+	}
+	else{
+		console.log("n checked");
+	}
+}
 
 //show hide list image
 $scope.visible=false;
@@ -17,36 +27,37 @@ $scope.affichage =function() {
 	else{
 	$scope.visible=false;
 	}
+
 	console.log("TESTCLIK");
 	$scope.names = [
-    {name:'Jani',country:'Norway',image :'images/image1.jpg'},
-    {name:'Hege',country:'Sweden'},
-    {name:'Kai',country:'Denmark'}
-];
+	    {name:'SAAD',country:'Mroc',age:'18',image :'images/image1.jpg'},
+	    {name:'MARK',country:'Sweden',age:'28'},
+	    {name:'OLIA',country:'Denmarkk',age:'36'} 
+	];
 }
+
 
 $scope.ajout =function() {
-	if($scope.myname)
+	if($scope.nom && $scope.country )
 	{
-		console.log($scope.myname);
-		var ta = {name:$scope.myname,country:'Sweden'};
+		console.log($scope.nom);
+		var ta = {name:$scope.nom,country:$scope.country};
 		$scope.names.push(ta);
-
 	}
-	
-
 }
-
 
 $scope.clikImg =function() 
 {
 	console.log("TESTCLIK");
 }
 
+$scope.num = 2;
+$scope.isNumber = angular.isNumber($scope.num);
 
-});
+  }]);
 
 
+///test myApp2
 
 angular.module('myApp2', []).controller('personCtrl', function($scope) {
     $scope.firstName = 'SAAD' ;
@@ -67,3 +78,5 @@ $scope.infoPerso = maFiche;
 
 
 });
+
+
